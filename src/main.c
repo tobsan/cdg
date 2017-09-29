b727b2eb99acf271aca8392c70c854f5e982a64f
Tobias Olausson <tobsan@tobsan.se>
2017-09-29 15:04:21 +0200

    Example code uses SDL
    SDL and debugging is added to the Makefile.
    
    Signed-off-by: Tobias Olausson <tobsan@tobsan.se>

--- src/main.c
+++ src/main.c
@@ -19,8 +19,78 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
+#include <SDL/SDL.h>
 #include "cdg.h"
+
+int cdg_read_file(char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error while opening file: %s\n", strerror(errno));
+        exit(2);
+    }
+
+    SubCode subcode;
+    cdg cdg_state;
+
+    // The SDL surface to write to
+    SDL_Surface* screen = SDL_SetVideoMode(CDG_SCREEN_WIDTH, CDG_SCREEN_HEIGHT, 32, SDL_SWSURFACE);
+
+    // While file has not ended
+    int p = 0;
+    while (fread(&subcode, sizeof(SubCode), 1, file)) {
+        CDG_Packet packet = cdg_parse_packet(&subcode);
+        if(cdg_process_packet(&packet, &cdg_state) != 0) {
+            printf("Something went wrong\n");
+            return 1;
+        }
+        // Free the packet
+        cdg_packet_put(&packet);
+
+        SDL_LockSurface(screen);
+        // Set the pixels in the SDL surface
+        for (int i = 0; i < CDG_SCREEN_WIDTH; i++) {
+            for (int j = 0; j < CDG_SCREEN_HEIGHT; j++) {
+                unsigned char color_index = cdg_state.pixels[i][j];
+                CDG_RGB color = cdg_state.color_table[color_index];
+
+                // R-G-B-A. 
+                Uint32 sdl_color = (((Uint32)color.red) << 24)
+                                 | (((Uint32)color.green) << 16)
+                                 | (((Uint32)color.blue << 8));
+
+                sdl_color *= 2;
+                /*
+                int offset = i * CDG_SCREEN_WIDTH + j;
+                Uint32 target_pixel = screen->pixels + offset
+                screen->pixels[offset] = 
+                */ 
+            }
+        }
+        SDL_UnlockSurface(screen);
+        printf("Handled packet %i\n", p++);
+
+        //Update the screen
+        if( SDL_Flip( screen ) == -1 ) { 
+            return 1;
+        }
+
+        // 10x speed
+        // SDL_Delay(CDG_MSECS_PER_PACKET / 10);
+    }
+
+    // Sanity check and close the file
+    if (!feof(file) || ferror(file)) {
+        fprintf(stderr, "Reading of file failed\n");
+        return 2;
+    }
+    fclose(file);
+    // Done doing file stuff
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -30,8 +100,11 @@ int main(int argc, char **argv)
         exit(1);
     }
 
+    SDL_Init( SDL_INIT_EVERYTHING );
+
     char *filename = argv[1];
     cdg_read_file(filename);
     
+    SDL_Quit();
     return 0;
 }
